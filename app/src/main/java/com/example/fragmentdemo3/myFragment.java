c965c5b0d0a6a718ce965257120c412f93108b2b
hhinkel <ad-astra@comcast.net>
2019-09-06 17:48:16 -0400

    Initial commit

---
+++ app/src/main/java/com/example/fragmentdemo3/myFragment.java
@@ -0,0 +1,19 @@
+package com.example.fragmentdemo3;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+public class myFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup viewGroup, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment, viewGroup, false);
+        TextView output = (TextView) view.findViewById(R.id.msg);
+        output.setText("Fragment");
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/fragmentdemo3/myListFragment.java
@@ -0,0 +1,52 @@
+package com.example.fragmentdemo3;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+import androidx.constraintlayout.solver.widgets.Helper;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.fragment.app.ListFragment;
+
+public class myListFragment extends ListFragment implements AdapterView.OnItemClickListener {
+
+    //@Override
+    public View OnCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.list_fragment, container, false);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated (Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(getActivity(), R.array.Planets, android.R.layout.simple_list_item_1);
+        setListAdapter(adapter);
+        getListView().setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        //Create fragment with index
+        Fragment fragment = new Fragment();
+        Bundle args = getArguments();
+        fragment.setArguments(args);
+        //Clear the back stack
+        FragmentManager fragmentManager = getFragmentManager();
+        int backStackEntryCount = fragmentManager.getBackStackEntryCount();
+        for (int i = 0; i < backStackEntryCount; i++) {
+            fragmentManager.popBackStackImmediate();
+        }
+        //Perform the transaction
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.msg, fragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+        //Toast.makeText(getActivity(), "Item: " + position, Toast.LENGTH_SHORT).show();
+}
